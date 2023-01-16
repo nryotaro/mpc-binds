ea057e4fe856c8836edb5af40dcd2c39c7cf2857
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-01-16 14:05:14 -0500

    temp

--- mpc-binds.el
+++ mpc-binds.el
@@ -1,9 +1,34 @@
-;;; package --- Summary key-bindings of mpc-mode.
+;;; package --- Summary key-bindings of mpc-mode. -*- lexical-binding: t -*-
 ;;; Commentary:
 
-;; The prefix key is m.
+;; The prefix key for mpc commands is "m".#<buffer mpc-binds.el>
 
 ;;; Code:
+(require 'mpc)
+
+(defun mpc-bindings--bind-mpc-commands ()
+  "Bind mpc commands to a keymap, returning the keymap."
+  (let ((keymap (make-sparse-keymap)))
+    (dolist (pair '(("p" . mpc-play)
+		    ("u" . mpc-update)))
+      (define-key keymap (car pair) (cdr pair)))
+    keymap))
+
+
+(defun mpc-bindings--select-window-by-buffer-name
+    (windows name)
+  "Select the widows with a NAME in WINDOWS."
+  (seq-filter
+   (lambda (w) (equal name (-> w window-buffer buffer-name)))
+   (window-list)))
+
+(defun mpc-bindings--bind-move-window
+    ()
+  "Bind moving to a mpc window to a keymap, returning the keymap."
+  (let ((keymap (make-sparse-keymap)))
+    1))
+
+(define-key mpc-mode-map "m" (mpc-bindings--bind-mpc-commands))
 
 (provide 'mpc-binds)
 ;;; mpc-binds.el ends here.
