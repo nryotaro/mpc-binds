9c8047c633c48f3722171ad7dc3e58ea0e7faa9a
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-01-16 14:31:09 -0500

    bindsPp

--- mpc-binds.el
+++ mpc-binds.el
@@ -6,7 +6,14 @@
 ;;; Code:
 (require 'mpc)
 (require 'dash)
-(defun mpc-bindings--bind-mpc-commands ()
+
+(defun mpc-bind-select-genre
+    ()
+  "Select *MPC Genre* window."
+  (interactive)
+  (mpc-bind--select-window-by-buffer-name "*MPC Genres*"))
+
+(defun mpc-bind--bind-mpc-commands ()
   "Bind mpc commands to a keymap, returning the keymap."
   (let ((keymap (make-sparse-keymap)))
     (dolist (pair '(("p" . mpc-play)
@@ -14,21 +21,30 @@
       (define-key keymap (car pair) (cdr pair)))
     keymap))
 
-(defun mpc-bindings--select-window-by-buffer-name
+(defun mpc-bind--find-window-by-buffer-name
     (windows name)
   "Select the widojws with a NAME in WINDOWS."
   (seq-filter
    (lambda (w) (equal name (-> w window-buffer buffer-name)))
    windows))
 
-(defun mpc-bindings--bind-move-window
+(defun mpc-bind--select-window-by-buffer-name
+    (name)
+  "Select the window whose buffer is NAME."
+  (when-let ((windows (mpc-bind--find-window-by-buffer-name (window-list) name)))
+    (select-window (car windows))))
+
+(defun mpc-bind--bind-move-window
     ()
   "Bind moving to a mpc window to a keymap, returning the keymap."
   (let ((keymap (make-sparse-keymap)))
+    (dolist (pair '(("g" . mpc-bind-select-genre)
+		    ))
+      (define-key keymap (car pair) (cdr pair)))
     keymap))
 
-(define-key mpc-mode-map "m" (mpc-bindings--bind-mpc-commands))
-(define-key mpc-mode-map "b" (mpc-bindings--bind-move-window))
+(define-key mpc-mode-map "m" (mpc-bind--bind-mpc-commands))
+(define-key mpc-mode-map "b" (mpc-bind--bind-move-window))
 
 (provide 'mpc-binds)
 ;;; mpc-binds.el ends here.
